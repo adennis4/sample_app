9b2838160caf838e481ac754b04c4089f335ffa6
Andrew Dennis <adennis4@gmail.com>
2012-03-11 22:57:54 -0700

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
+  
   before_save :create_remember_token
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -8,6 +10,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   private
   
